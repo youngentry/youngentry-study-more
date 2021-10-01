1d9f11a29a59fa851b34af6aabb7d0ebccfcf686
youngentry <90388461+youngentry@users.noreply.github.com>
2021-10-01 20:32:10 +0900

    js-lesson__finished
    From the next commit, I'm going to make designs of the web page.

--- js/todo.js
+++ js/todo.js
@@ -1,47 +1,52 @@
-const todoForm = document.querySelector("#todo-form");
-const todoInput = document.querySelector("#todo-form input ");
-const todoList = document.querySelector("#todo-list");
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-function deleteTodo(event) {
+function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos()
 }
 
-function paintTodo(newTodo) {
+function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.addEventListener("click", deleteTodo);
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-  button.innerText = "X";
-  todoList.appendChild(li);
+  toDoList.appendChild(li);
 }
-
 function handleToDoSubmit(event) {
   event.preventDefault();
-  const newTodo = todoInput.value;
-  todoInput.value = "";
-  toDos.push(newTodo);
-  paintTodo(newTodo);
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
-todoForm.addEventListener("submit", handleToDoSubmit);
-
+toDoForm.addEventListener("submit", handleToDoSubmit);
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
-if (savedToDos !== null);
-{
+if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
-  parsedToDos.forEach((item) => console.log("this is turn of", item));
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const API_KEY = "13c81c0431b5f3dc9fbf82b6c53e3520";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} ${data.main.temp.toFixed(
+        1
+      )}°C`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
