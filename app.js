bb67d2b456e8cb6f503411c055a27292e9d28fd6
youngentry <90388461+youngentry@users.noreply.github.com>
2021-10-21 19:54:44 +0900

    made stroke functions

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
+
+canvas.width = 800;
+canvas.height = 400;
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
 
 function onMouseMove(event) {
-  console.log(event);
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function onMouseDown(event) {
+  painting = true;
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
