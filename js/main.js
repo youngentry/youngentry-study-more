35030d8f565dde5dd81c44c16ae10779ac4502ff
youngentry <gentry_@naver.com>
2022-06-20 17:24:01 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function () {
+  $(".topBanner i").on("click", function () {
+    $(".topBanner").slideUp();
+  });
+
+  $(".mainSlider").on("init afterChange", function (e, s, c) {
+    var current = $(".slick-current");
+    current.addClass("on").siblings().removeClass("on");
+    $(".slideNum").text((c ? c + 1 : 1) + " / " + s.slideCount);
+    $(".mainArrow i").removeClass("on");
+    if (c == 1) {
+      $(".mainArrow i").addClass("on");
+    }
+  });
+
+  $(".topBanner").on("click", function () {
+    $("#header").addClass("on");
+  });
+
+  $(".mainSlider").slick({
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  });
+
+  $(".mainArrow i:nth-child(1").on("click", function () {
+    $(".mainSlider)".slick("slickPrev"));
+  });
+  $(".mainArrow i:nth-child(2").on("click", function () {
+    $(".mainSlider)".slick("slickNext"));
+  });
+});
