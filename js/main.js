a48114b29e202bfaaf9850a34dfac51a9a014655
youngentry <gentry_@naver.com>
2022-06-22 13:03:44 +0900

    220622 responsive_iframe_and_kakaomap

--- js/main.js
+++ js/main.js
@@ -31,4 +31,19 @@ $(function () {
   $(".mainArrow i:nth-child(2").on("click", function () {
     $(".mainSlider)".slick("slickNext"));
   });
+
+  $("#utb").YTPlayer({
+    videoURL: "https://youtu.be/dS9x5Ws67wI",
+    containment: ".utb",
+    autoPlay: true,
+    mute: true,
+    startAt: 0,
+    opacity: 1,
+    showControls: false,
+    playOnlyIfVisible: true,
+  });
+  $(".utb .zoom").on("click", function (e) {
+    e.preventDefault();
+    $("#utb").YTPFullscreen();
+  });
 });
