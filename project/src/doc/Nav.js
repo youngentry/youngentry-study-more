201e33e0158ff26987711026633d2e9576c930bc
youngentry <gentry_@naver.com>
2022-08-05 10:04:45 +0900

    220805

--- project/src/doc/Nav.js
+++ project/src/doc/Nav.js
@@ -1,16 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <nav className="Nav gnb">
-            <ul>
-                <li><Link to="/section01">main</Link></li>
-                <li><Link to="/section01">sc01</Link></li>
-                <li><Link to="/section02">sc02</Link></li>
-                <li><Link to="/section03">sc03</Link></li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="Nav gnb">
+      <ul>
+        <li>
+          <Link to="/">main</Link>
+        </li>
+        <li>
+          <Link to="/section01">sc01</Link>
+        </li>
+        <li>
+          <Link to="/section02">sc02</Link>
+        </li>
+        <li>
+          <Link to="/section03">sc03</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
---
+++ project/src/doc/Section04.js
@@ -0,0 +1,21 @@
+const Section04 = () => {
+  return (
+    <section className="About02 basicSection">
+      <h2>
+        <span>CEO's</span>GREETINGS
+      </h2>
+      <p>항상 새로운 영투</p>
+      <div className="inner">
+        <div className="top">
+          <figure>
+            <img src="./img/main01.jpg" alt="" />
+          </figure>
+        </div>
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Section04;
