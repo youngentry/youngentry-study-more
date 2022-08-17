cf524c09a819617731a57a8f79abd3f3269bf8da
youngentry <gentry_@naver.com>
2022-08-17 17:49:05 +0900

    220817

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("scroll", () => {
+    let sct = window.scrollY;
+    console.log(sct);
+    sct > 0 ? document.querySelector("#Header").classList.add("on") : document.querySelector("#Header").classList.remove("on");
+  });
 
+  new Swiper(".MainSlider", {
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false, // 호버, 포커스에도 멈추지 않도록 합니다.
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
-    const SCE_ELE = document.querySelectorAll('.screvent');
-
-    window.addEventListener('scroll', () => {
-        let sct = window.scrollY;
-        console.log(sct);
-        sct > 0
-            ? document.querySelector('#Header').classList.add('on')
-            : document.querySelector('#Header').classList.remove('on')
+  const SCE_ELE = document.querySelectorAll(".active-event");
+  window.addEventListener("scroll", () => {
+    let sct = window.scrollY;
+    SCE_ELE.forEach((el) => {
+      sct > el.offsetTop - 300 ? el.classList.add("on") : el.classList.remove("on");
     });
+  });
 
-    window.addEventListener('scroll', () => {
-        let sct = window.scrollY;
-        SCE_ELE.forEach(el => {
-            sct > el.offsetTop
-                ? el.classList.add('on')
-                : el.classList.remove('on')
-        })
-    });
-
-    const UL = document.querySelector('nav ul');
-    const LI = [...UL.children][1];
-    console.log(LI);
-
-
-
-
-
-})
+  // const UL = document.querySelector('nav ul');
+  // const LI = [...UL.children][1];
+  // console.log(LI);
+});
