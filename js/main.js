a75b7b0934dd40afccd683327f0fe93ff0f2f8be
youngentry <gentry_@naver.com>
2022-08-17 10:04:43 +0900

    220817

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const H1 = document.querySelector("h1");
-  H1.addEventListener("click", (e) => {
-    e.currentTarget.style.color = `blue`;
-  });
+window.addEventListener('DOMContentLoaded', () => {
 
-  const swiper = new Swiper(".swiper", {
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-    on: {
-      // init: function () {
-      //   console.log("swiper initialized");
-      // },
-      slideChangeTransitionEnd: function () {
-        console.log(this.slides.length, this.realIndex);
 
-        document.querySelectorAll(".swiper-slide").forEach((e) => e.classList.remove("on"));
+    const SCE_ELE = document.querySelectorAll('.screvent');
 
-        document.querySelector(".swiper-slide-active").classList.add("on");
-        document.querySelector("h1 strong").innerText = `${this.realIndex + 1} / ${this.slides.length - 2}`;
-      },
-    },
-  });
-});
+    window.addEventListener('scroll', () => {
+        let sct = window.scrollY;
+        console.log(sct);
+        sct > 0
+            ? document.querySelector('#Header').classList.add('on')
+            : document.querySelector('#Header').classList.remove('on')
+    });
+
+    window.addEventListener('scroll', () => {
+        let sct = window.scrollY;
+        SCE_ELE.forEach(el => {
+            sct > el.offsetTop
+                ? el.classList.add('on')
+                : el.classList.remove('on')
+        })
+    });
+
+    const UL = document.querySelector('nav ul');
+    const LI = [...UL.children][1];
+    console.log(LI);
+
+
+
+
+
+})
