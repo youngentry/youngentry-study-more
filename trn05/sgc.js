d83b6b85ff241ede844282a275ca793f8ffd1371
youngentry <gentry_@naver.com>
2022-08-19 10:47:28 +0900

    220819

--- trn05/sgc.js
+++ trn05/sgc.js
@@ -1,29 +1,25 @@
 $(function () {
-  var TXT = ["HOME", "01", "02", "03", "04", "young", "copyright"];
-  $(".main").fullpage({
-    anchors: ["page01", "page02", "page03", "page04", "page05", "footer"],
-    //navigation: true,
-    css3: false,
-    afterLoad: function (page, num) {
-      setTimeout(() => {
-        $(".section")
-          .eq(num - 1)
-          .addClass("on")
-          .siblings()
-          .removeClass("on");
-      });
-      $(".navBar li")
-        .eq(num - 1)
-        .addClass("on")
-        .siblings()
-        .removeClass("on");
-      $(".this_page").text(TXT[num - 1]);
+    var TXT = ["HOME", "01", "02", "03", "04", "young", "copyright"];
+    $(".main").fullpage({
+        anchors: ["page01", "page02", "page03", "page04", "page05", "footer"],
+        //navigation: true,
+        css3: false,
+        afterLoad: function (page, num) {
+            setTimeout(() => {
+                $(".section")
+                    .eq(num - 1)
+                    .addClass("on")
+                    .siblings()
+                    .removeClass("on");
+            });
+            $(".navBar li")
+                .eq(num - 1)
+                .addClass("on")
+                .siblings()
+                .removeClass("on");
+            $(".this_page").text(TXT[num - 1]);
 
-      if (num !== 1) {
-        $(".Header").addClass("on");
-      } else {
-        $(".Header").removeClass("on");
-      }
-    },
-  });
+            !num !== 1 ? $(".Header").addClass("on") : $(".Header").removeClass("on");
+        },
+    });
 });
