cf5c7c181e8f506dbf491b2ad4e37fc4cd812d00
youngentry <gentry_@naver.com>
2022-08-22 09:50:35 +0900

    220822

--- trn05/sgc.js
+++ trn05/sgc.js
@@ -1,25 +1,28 @@
 $(function () {
-    var TXT = ["HOME", "01", "02", "03", "04", "young", "copyright"];
-    $(".main").fullpage({
-        anchors: ["page01", "page02", "page03", "page04", "page05", "footer"],
+    var TXT = ['HOME', '01', '02', '03', '04', 'lee,s portfolio', 'copyright']
+    $('.main').fullpage({
+        anchors: ['page01', 'page02', 'page03', 'page04', 'page05', 'footer'],
         //navigation: true,
         css3: false,
         afterLoad: function (page, num) {
-            setTimeout(() => {
-                $(".section")
-                    .eq(num - 1)
-                    .addClass("on")
-                    .siblings()
-                    .removeClass("on");
+            //애니메이션을 큐스텍에 담아서 딜레이 시킴...
+            setTimeout(function () {
+                $('.section').eq(num - 1).addClass('on').siblings().removeClass('on');
             });
-            $(".navBar li")
-                .eq(num - 1)
-                .addClass("on")
-                .siblings()
-                .removeClass("on");
-            $(".this_page").text(TXT[num - 1]);
+            $('.navBar li').eq(num - 1).addClass('on').siblings().removeClass('on');
+            $('.this_page').text(TXT[num - 1]);
 
-            !num !== 1 ? $(".Header").addClass("on") : $(".Header").removeClass("on");
+            //부정연산자 사용
+            num !== 1
+                ? $('.Header').addClass('on')
+                : $('.Header').removeClass('on')
         },
     });
-});
+
+
+    $('.basicSlider').slick({
+        arrows: false,
+        asNavFor: '.basicSlider',
+    });
+
+})
