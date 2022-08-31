f6ab98863ed510605273d2fcb9ae705eda54c59a
youngentry <gentry_@naver.com>
2022-08-31 09:17:08 +0900

    220831

--- js/main.js
+++ js/main.js
@@ -5,9 +5,31 @@ $(function () {
         $('.Section01').addClass('on');
     });
 
+    $('.main_slider').on('init reInit afterChange', function (e, ms, c) {
+        var current = $('.main_slider .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+        $('.slide_dots li').eq(c).addClass('on').siblings().removeClass('on');
+        $('.Section01 .num').html('0' + ((c ? c : 0) + 1) + '<span> / 0' + ms.slideCount + '</span>');
+    });
+
+    $('.Section01 .slide_dots li').on('click', function () {
+        var idx = $(this).index();
+        console.log(idx)
+        $('.main_slider').slick('slickGoTo', idx)
+    })
+
     $('.main_slider').slick({
         arrows: false,
     });
+
+    $('.Section01 .slide_handler button:first-child').on('click', function () {
+        $('.main_slider').slick('slickPrev')
+    })
+    $('.Section01 .slide_handler button:last-child').on('click', function () {
+        $('.main_slider').slick('slickNext')
+    })
+
+
 
     $('.left_slider').slick({
         arrows: false,
@@ -25,16 +47,27 @@ $(function () {
         var current = $('.center_slider .slick-current');
         current.addClass('on').siblings().removeClass('on');
         console.log(current);
-        $('.num').html((c ? c : 0) + 1 + '<span> / 0' + s.slideCount + '</span>');
+        $('.Section04 .num').html((c ? c : 0) + 1 + '<span> / 0' + s.slideCount + '</span>');
         $('.content_box>div').eq(c).addClass('on').siblings().removeClass('on');
     });
 
+
+
+
     $('.center_slider').slick({
         arrows: false,
+        dots: true,
         centerMode: true,
         centerPadding: '300px',
-
     });
+
+
+    $('.Section04 .slide_handler i:first-child').on('click', function () {
+        $('.center_slider').slick('slickPrev')
+    });
+    $('.Section04 .slide_handler i:last-child').on('click', function () {
+        $('.center_slider').slick('slickNext')
+    })
 
 
 
@@ -50,6 +83,25 @@ $(function () {
         sct > 0
             ? $('.Header').addClass('on')
             : $('.Header').removeClass('on')
+    });
+
+    // pop up cookie
+
+
+    var date = new Date();
+    date.setTime(date.getTime() + 60 * 1000);
+
+    console.log(date)
+    console.log($.cookie('todayPopUp'));
+    var TCK = $.cookie('todayPopUp');
+    if (!TCK) {
+        $('.today_pop').show();
+    }
+
+    $('#Today').on('change', function () {
+        $.cookie('todayPopUp', '팝업쿠키', { expires: date });
+        $('.today_pop').hide();
+        //출처: https://offbyone.tistory.com/176 [쉬고 싶은 개발자:티스토리]
     })
 
 
