44bf583e03707e4c57f3422ddc68fa92b4323ff3
youngentry <gentry_@naver.com>
2022-09-07 16:05:21 +0900

    220907

--- src/App.js
+++ src/App.js
@@ -1,7 +1,67 @@
+import { useState } from "react";
 import "./App.css";
 
+const DB = [
+    {
+        id: 1,
+        content: "menu01",
+        link: "/",
+        submenu: [
+            { content: "submenu01", link: "/1" },
+            { content: "submenu02", link: "/2" },
+            { content: "submenu03", link: "/3" },
+        ],
+    },
+    {
+        id: 2,
+        content: "menu02",
+        link: "/",
+        submenu: [
+            { content: "submenu01", link: "/1" },
+            { content: "submenu02", link: "/2" },
+            { content: "submenu03", link: "/3" },
+        ],
+    },
+    {
+        id: 3,
+        content: "menu03",
+        link: "/",
+        submenu: [
+            { content: "submenu01", link: "/1" },
+            { content: "submenu02", link: "/2" },
+            { content: "submenu03", link: "/3" },
+        ],
+    },
+];
+
 function App() {
-    return <>README가 왜 뜰까</>;
+    const [CB, setCB] = useState("");
+    const [TG, setTG] = useState(false);
+    return (
+        <header>
+            <h1 className={CB} onClick={() => setCB("on")}>
+                LOGO
+            </h1>
+            <h2 className={`abc ${TG ? "on" : ""}`}>Toggle Class</h2>
+            <button onClick={() => setTG(!TG)}>Class 토글</button>
+            <nav className="GNB">
+                <ul>
+                    {DB.map((it, idx) => (
+                        <li key={idx}>
+                            <a href={it.link}>{it.content}</a>
+                            <ul className="smenu">
+                                {it.submenu.map((smenu, idx) => (
+                                    <li key={idx}>
+                                        <a href={smenu.link}>{smenu.content}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default App;
