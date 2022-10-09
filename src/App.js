c6eb12c67c17ef31f04c675c7b5a787b1dc935c7
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-09 13:39:42 +0900

    221009

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+
+const App = () => {
+    return (
+        <div className="App">
+            <DiaryEditor />
+        </div>
+    );
+};
+export default App;
