cfdc26eecfd728484addc3e1e27e7d6c9e0691b0
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-09 14:46:33 +0900

    221009

--- src/ItemDiary.js
+++ src/ItemDiary.js
@@ -2,17 +2,26 @@ import React from "react";
 import "./css/ItemDiary.scss";
 
 const ItemDiary = ({ diaryList, removeHandler }) => {
+    const currentDate = new Date().getTime();
+    const diaryDate = new Date(currentDate).toLocaleDateString();
+
     return (
-        <div>
+        <div className="ItemDiary">
             {diaryList.map((el, idx) => {
                 return (
                     <div className="box" key={el.id}>
                         <ul>
-                            <li className="listId">작성 번호 : {el.id}</li>
-                            <li className="title">제목 : {el.title}</li>
+                            <div className="titleArea">
+                                <li className="title">제목 : {el.title}</li>
+                                <li className="listId">작성 번호 : {el.id}</li>
+                            </div>
+                            <li className="date">작성 날짜 : {diaryDate}</li>
                             <li className="content">내용 : {el.content}</li>
                         </ul>
-                        <button onClick={() => removeHandler(el.id)}>삭제하기</button>
+                        <div className="buttonArea">
+                            <button onClick={() => removeHandler(el.id)}>수정하기</button>
+                            <button onClick={() => removeHandler(el.id)}>삭제하기</button>
+                        </div>
                     </div>
                 );
             })}
