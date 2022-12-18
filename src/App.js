7abee95a1ec9e4919026394ac2aa4161060e2e95
youngentry <90388461+youngentry@users.noreply.github.com>
2022-12-19 03:22:35 +0900

    feat: 상품 데이터 받아와서 출력하기

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
-import "./App.css";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import "./css/App.scss";
+import { Container, Nav, Navbar, Row, Col } from "react-bootstrap";
+import PRODUCT_DATA from "./data/product.js";
+import { useState } from "react";
 
 function App() {
+    const [shoes] = useState(PRODUCT_DATA);
     return (
         <div className="App">
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Y MALL</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-            <div className="mainVisual"></div>
-            <Container>
-                <Row>
-                    <Col>1 of 3</Col>
-                    <Col>2 of 3</Col>
-                    <Col>3 of 3</Col>
-                </Row>
-            </Container>
+            <NavBar />
+            <MainVisual />
+            <MainProduct shoes={shoes} />
         </div>
     );
 }
+
+const NavBar = () => (
+    <Navbar bg="dark" variant="dark">
+        <Container>
+            <Navbar.Brand href="#home">Y MALL</Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+        </Container>
+    </Navbar>
+);
+
+const MainVisual = () => <div className="mainVisual"></div>;
+
+const MainProduct = ({ shoes }) => (
+    <Container className="mainProduct">
+        <Row>
+            {/* {shoes.map((product, index) => (
+                <Card key={product.id} product={product} productIndex={index} /> */}
+            {shoes.map((_, index) => (
+                // 데이터를 전달할 때부터 원본 데이터의 형태를 그대로 사용하여
+                // el로 사람 헷갈리에 만들지 말고 data[index] 형태로 처리한다.
+                <Card key={shoes[index].id} shoes={shoes[index]} index={index} />
+            ))}
+        </Row>
+    </Container>
+);
+
+const Card = ({ shoes, index }) => (
+    <Col>
+        <img src={process.env.PUBLIC_URL + `/img/product0${index + 1}.jpg`} alt="" />
+        <h4>{shoes.title}</h4>
+        <p>{shoes.content}</p>
+        <p>{shoes.price}</p>
+    </Col>
+);
 
 export default App;
