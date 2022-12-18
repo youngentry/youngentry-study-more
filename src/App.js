6c149b64edc67c401c2357fc00b76592823908a9
youngentry <90388461+youngentry@users.noreply.github.com>
2022-12-18 22:35:14 +0900

    feat: creat react app

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Y MALL</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <div className="mainVisual"></div>
+            <Container>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col>2 of 3</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default App;
